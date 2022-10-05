1872d5591013acf7f080e791e58004ea3f5fc13e
Anshuman Tiwari <anshuman21153045@akgec.ac.in>
2022-10-05 10:09:06 +0530

    added 3 modes and enabled switched between them

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var i=0;
-function x(){
-    i--;
-    document.getElementById("background").style.backgroundPosition=i+"px";
+function takemetospace()
+{
+    document.getElementById("all").classList.remove("normalbackground");
+    document.getElementById("all").classList.remove("backgroundsky");
+
+    const list = document.getElementById("all").classList;
+    list.add("background");
+
+//    var i=0;
+//    function x(){
+//       i--;
+//       document.getElementsByClassName("background").style.backgroundPosition=i+"px";
+//    }
+//    setInterval(x,5);
 }
-setInterval(x,5);
+function takemetosky()
+{
+    document.getElementById("all").classList.remove("normalbackground");
+    document.getElementById("all").classList.remove("background");
+    const list = document.getElementById("all").classList;
+    list.add("backgroundsky");
+
+
+}
+function talknojutsu()
+{
+    document.getElementById("all").classList.remove("backgroundsky");
+    document.getElementById("all").classList.remove("background");
+    const list = document.getElementById("all").classList;
+    list.add("normalbackground");
+}
