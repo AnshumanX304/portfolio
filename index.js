389d8d9c7d8c2a3e92d55d9aa7d524591431e22d
Anshuman Tiwari <anshuman21153045@akgec.ac.in>
2022-10-05 20:35:42 +0530

    movements added to the animations

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+var idnight=NULL;
+var idday=NULL;
+var idday2=null;
 function takemetospace()
 {
     document.getElementById("all").classList.remove("normalbackground");
     document.getElementById("all").classList.remove("backgroundsky");
-
     const list = document.getElementById("all").classList;
     list.add("background");
 
-//    var i=0;
-//    function x(){
-//       i--;
-//       document.getElementsByClassName("background").style.backgroundPosition=i+"px";
-//    }
-//    setInterval(x,5);
+    var i=0;
+    clearInterval(idnight);
+    idnight=setInterval(x,10);
+    function x()
+    {
+        document.querySelector(".background").style.backgroundPosition=i+'px';
+        i--;
+    }
+    
 }
 function takemetosky()
 {
@@ -20,6 +25,17 @@ function takemetosky()
     const list = document.getElementById("all").classList;
     list.add("backgroundsky");
 
+    var i=0;
+
+    clearInterval(idday);
+    idday=setInterval(x,10);
+    function x()
+    {
+        document.querySelector(".backgroundsky").style.backgroundPosition=i+'px';
+        i--;
+    }
+
+    
 
 }
 function talknojutsu()
@@ -28,4 +44,17 @@ function talknojutsu()
     document.getElementById("all").classList.remove("background");
     const list = document.getElementById("all").classList;
     list.add("normalbackground");
+    
+}
+function movesky()
+{
+    var i=0;
+    clearInterval(idday2);
+    idday2=setInterval(x,10);
+    function x()
+    {
+        document.querySelector(".container1").style.backgroundPositionY = i+'px'; 
+        i++;
+    }
+   
 }
